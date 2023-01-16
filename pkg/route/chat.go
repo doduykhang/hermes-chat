b3b424a224bcb7f9e6ea09529190b8dd699732c9
khang <dokhang295@gmail.com>
2023-01-16 18:52:03 +0700

    hello endpoint

--- pkg/route/chat.go
+++ pkg/route/chat.go
@@ -1,6 +1,8 @@
 package route
 
 import (
+	"net/http"
+
 	"github.com/doduykhang/hermes/chat/pkg/handler"
 	"github.com/go-chi/chi/v5"
 )
@@ -8,4 +10,7 @@ import (
 func ChatRoute(r chi.Router, h *handler.Chat) {
 	go h.HandleMessage()
 	r.Get("/ws/{roomId}", h.HandleConnect)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	})
 }
